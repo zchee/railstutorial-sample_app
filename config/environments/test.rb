bcea968035fb5d6f2c220e8c9d27082e3645e29d
zchee <z@zsh.us>
2014-12-03 19:35:32 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  # bcryptのコスト変数っを下げることでテストの速度を向上させる
+  ActiveModel::SecurePassword.min_cost = true
 end
