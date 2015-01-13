4712c4ac405db6f727fea11c78c3d1fde5f13950
zchee <zchee@zsh.us>
2015-01-14 02:10:46 +0900

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe 'Static pages' do
 
   subject { page }
 
-  describe "Home page" do
+  describe 'Home page' do
     before { visit root_path }
 
     it { should have_content('Sample App') }
@@ -14,8 +14,8 @@ describe "Static pages" do
     describe 'for signed-in users' do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: 'Lorem ipsum')
-        FactoryGirl.create(:micropost, user: user, content: 'Dolor sit amet')
+        FactoryGirl.create(:micropost, user: user, content: 'Lorem')
+        FactoryGirl.create(:micropost, user: user, content: 'Ipsum')
         sign_in user
         visit root_path
       end
@@ -25,24 +25,35 @@ describe "Static pages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe 'follower/following counts' do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link('0 following', href: following_user_path(user)) }
+        it { should have_link('1 followers', href: followers_user_path(user)) }
+      end
     end
   end
 
-  describe "Help page" do
+  describe 'Help page' do
     before { visit help_path }
 
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
 
-  describe "About page" do
+  describe 'About page' do
     before { visit about_path }
 
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
   end
 
-  describe "Contact page" do
+  describe 'Contact page' do
     before { visit contact_path }
 
     it { should have_content('Contact') }
